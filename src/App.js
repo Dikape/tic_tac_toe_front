ebb62c4d013dd88424880daf0e157fe9c844241a
Dmytro Kasianchyk <dikape03@gmail.com>
2018-02-13 20:45:27 +0200

    add routers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import { Login, Register } from './components/auth';
+
+const MainRoutes = () => (
+  <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/register' component={Register} />
+  </Switch>
+)
+const Header = () => (
+  <header className="App-header">
+    <Link to='/'><h1 className="App-title">New game</h1></Link>
+    <h1 className="App-title">Tic-tac-toe by default react template</h1>
+    <Link to='/login'><button className="btn btn-primary auth-btn">Login</button></Link>
+    <Link to='/register'><button className="btn btn-primary auth-btn">Register</button></Link>
+  </header>
+  )
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Router>
+        <div className="App">
+          <Header/>             
+          <MainRoutes/>   
+        </div>
+    </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
